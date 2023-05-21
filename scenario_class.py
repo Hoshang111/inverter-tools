4ec18e2a047334ed179087a0d9074a60f3cef130
matthew.gollasch@suncable.energy <matthew.gollasch@suncable.energy>
2023-05-21 07:34:13 +0000

    Re-architected Assumption class
    Pivoted the assumption data in a way that made sense. Re-architected the dateTime filter in preparation for a new "re-filter" button. The intention is that the re-filter button will not have to re-query the whole scenario, just re-create the pandas df. Fixed a bug where an XOR of start/end would crash the system.

--- scenario_class.py
+++ scenario_class.py
@@ -61,7 +61,7 @@ class Scenario:
         '''
         print("Scenario Loading, this may take several seconds...")
         self.optimisation = Optimisation(self.name, self.start, self.end, self.timezone)
-        self.assumption = Assumption(self.name, self.start, self.end)
+        self.assumption = Assumption(self)
         clear_output()
         print("Scenario Loaded")
         self.print_scenario_details()
@@ -243,7 +243,9 @@ class Optimisation:
 class Assumption:
     """
     A class to represent a given scenarios' assumption table.
+
     ...
+
     Attributes
     ----------
     name : str
@@ -252,38 +254,68 @@ class Assumption:
         start time of the queried assumption table
     end : str
         end time of the queried assumption table
+
     Methods
     -------
     create_assumption_df():
         Creates an assumption table for the scenario
     """
-    def __init__(self, scenario, start, end):
+    def __init__(self, scenario):
         '''
         Initialises Assumption Class
+
         Args:
             name (str) : name of the assumption scenario
             start (str) : start time of the assumption scenario
             end (str) : end time of the assumption scenario
+
         Returns:
             None
         '''
         self.scenario = scenario
-        self.start = start
-        self.end = end
-        self.df = self.create_assumption_df()
+        self.df_num = self.create_assumption_df()
+        self.filter_df()
 
     def create_assumption_df(self):
         '''
         Creates Pandas Dataframe for assumption table
+
         Args:
             None
+
         Returns:
-            df_assumptions (pandas dataframe object) : Dataframe that gives assumptions of scenario
+            df_assumptions_num (pandas dataframe object) : Dataframe that gives assumptions of scenario
         '''
-        scenario_assumptions_query = "select * from hive_metastore.federated_postgres.federated_optimisations_assumptions where scenario = '" + self.scenario + "' order by period_start asc"
+        scenario_assumptions_query = "select * from hive_metastore.federated_postgres.federated_optimisations_assumptions where scenario = '" + self.scenario.name + "' order by period_start asc"
         df_assumptions = spark.sql(scenario_assumptions_query).toPandas()
         df_assumptions['period_start']= pd.to_datetime(df_assumptions['period_start'])
         df_assumptions['period_end']= pd.to_datetime(df_assumptions['period_end'])
-        if self.start != None or self.end != None:
-            df_assumptions = df_assumptions.loc[(df_assumptions['period_start'] >= self.start) & (df_assumptions['period_end'] <= self.end)]  
-        return df_assumptions
+
+        df_assumptions_num = df_assumptions[df_assumptions['string_value'] != None]
+        df_assumptions_str = df_assumptions[df_assumptions['string_value'] == None]
+
+
+        # Pivot on period_start, fill forward any NA with last good value or 0.
+        df_assumptions_num = df_assumptions_num.pivot(index='period_start',columns='identifier', values='numeric_value').fillna(method='ffill').fillna(value=0)
+
+        return df_assumptions_num
+    
+    def filter_df(self):
+        '''
+        Filters the assumptions df based on user gui inputs. Designed to prevent re-querying of the scenario dbs.
+        Assigns to self.df.
+
+        Args:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Filter based on start/end time.
+        df_filter = self.df_num
+        if self.scenario.start != None:
+            df_filter = self.df_num.loc[self.scenario.start:] 
+        if self.scenario.end != None:
+            df_filter = self.df_num.loc[:self.scenario.end]  
+        self.df = df_filter
