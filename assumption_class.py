0a4bba8a722df668f5b7d2411bb40f253a7df5ab
matthew.gollasch@suncable.energy <matthew.gollasch@suncable.energy>
2023-05-21 06:38:45 +0000

    Updated assumptions_class

--- assumption_class.py
+++ assumption_class.py
@@ -35,7 +35,8 @@ class Assumption:
         self.scenario = scenario
         self.start = start
         self.end = end
-        self.df = self.create_assumption_df()
+        self.df_num = self.create_assumption_df()
+        self.filter_df(self.start, self.end)
 
     def create_assumption_df(self):
         '''
@@ -45,12 +46,36 @@ class Assumption:
             None
 
         Returns:
-            df_assumptions (pandas dataframe object) : Dataframe that gives assumptions of scenario
+            df_assumptions_num (pandas dataframe object) : Dataframe that gives assumptions of scenario
         '''
         scenario_assumptions_query = "select * from hive_metastore.federated_postgres.federated_optimisations_assumptions where scenario = '" + self.scenario + "' order by period_start asc"
         df_assumptions = spark.sql(scenario_assumptions_query).toPandas()
         df_assumptions['period_start']= pd.to_datetime(df_assumptions['period_start'])
         df_assumptions['period_end']= pd.to_datetime(df_assumptions['period_end'])
-        if self.start != None or self.end != None:
-            df_assumptions = df_assumptions.loc[(df_assumptions['period_start'] >= self.start) & (df_assumptions['period_end'] <= self.end)]  
-        return df_assumptions
+
+        df_assumptions_num = df_assumptions[df_assumptions['string_value'] = None]
+        df_assumptions_str = df_assumptions[df_assumptions['string_value'] != None]
+
+        # Pivot on period_start, fill forward any NA with last good value or 0.
+        df_assumptions_num = pd.pivot(index='period_start',columns='identifier', values='numeric_value').fillna(method='ffill').fillna(value=0)
+
+        return df_assumptions_num
+    
+    def filter_df(self, start, end):
+        '''
+        Filters the assumptions df based on user gui inputs. Designed to prevent re-querying of the scenario dbs.
+        Assigns to self.df.
+
+        Args:
+            start (str) : start time of the scenario
+            end (str) : end time of the scenario
+
+        Returns:
+            None
+        '''
+
+        # Filter based on start/end time.
+        if start != None or end != None:
+            df_filter = self.df_num[start:end]   
+
+        self.df = df_filter
